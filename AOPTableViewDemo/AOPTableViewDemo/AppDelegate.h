a8b46c836ed613d3046d0d6f531e469068c738e8
ljh <lijianghuai@linggan.com>
2016-06-01 14:35:31 +0800

    add demo & fix public_header_files

---
+++ AOPTableViewDemo/AOPTableViewDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  AOPTableView
+//
+//  Created by ljh on 16/5/30.
+//  Copyright © 2016年 ljh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
---
+++ AOPTableViewDemo/AOPTableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AOPTableView
+//
+//  Created by ljh on 16/5/30.
+//  Copyright © 2016年 ljh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
